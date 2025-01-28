dd88dbf030691e73f48aeb229871738597e39396
Habib <adebayohabib7@gmail.com>
2025-01-28 13:54:56 -0800

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,9 +12,9 @@ const About = () => {
 				<span className="block px-8 py-3 border-b-2 border-purple-500 text-black font-bold text-left text-[24px]">
 					About Me
 				</span>
-				<p className="text-[18px] mt-[20px] leading-[1.5] font-medium text-left">
+				<p className="text-[16px] mt-[20px] leading-[1.5] font-medium text-left">
 					I'm{" "}
-					<span className="text-purple-500 text-center font-bold">Habib</span>,
+					<span className="text-purple-500 text-center font-bold">Habib Adebayo</span>,
 					a Junior Frontend Developer with a passion for crafting engaging web
 					experiences. Proficient in{" "}
 					<span
